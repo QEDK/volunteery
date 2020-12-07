0e2d2ae67c36edce1ec1e542b9924db72cc12a94
Imperial-lord <absatyaprakash01@gmail.com>
2020-12-08 04:08:11 +0530

    :tada: feat: complete google-signin

--- social_good/lib/globals/myColors.dart
+++ social_good/lib/globals/myColors.dart
@@ -4,8 +4,13 @@ class MyColors {
   static const Color black = Colors.black;
   static const Color white = Colors.white;
   static const Color primaryColor = Color(0xFFE65933);
+  static const Color accentColor = Color(0xFF334155);
   static const Color backgroundColor = Color(0xFFE5E5E5);
 
   // shades of yellow
   static const Color yellowPrimary = Color(0xFFFED535);
+  static const Color yellowLight = Color(0xFFFEE272);
+
+  // shades of orange
+  static const Color orangeLight = Color(0xFFF09B85);
 }
